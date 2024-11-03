cb6dfb44076b20e34e36bce6f015a4739465a25e
GreaTuna <great.tuna.2020@gmail.com>
2024-11-03 17:47:18 +0200

    added image util

--- src/main/java/org/example/webstore/global/validation/annotation/Image/Base64ImageValidator.java
+++ src/main/java/org/example/webstore/global/validation/annotation/Image/Base64ImageValidator.java
@@ -3,15 +3,14 @@ package org.example.webstore.global.validation.annotation.Image;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import java.util.Base64;
-import java.util.regex.Pattern;
 import org.example.webstore.image.ImageConfig;
 import org.example.webstore.image.ImageType;
+import org.example.webstore.image.ImageUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class Base64ImageValidator implements ConstraintValidator<Base64Image, String> {
     private int maxSize;
     @Autowired private ImageConfig imageConfig;
-    private static final Pattern BASE64_IMAGE_PATTERN = Pattern.compile("data:image\\/[^;]+;base64[^\"]+");
 
     @Override
     public void initialize(Base64Image constraintAnnotation) {
@@ -23,32 +22,18 @@ public class Base64ImageValidator implements ConstraintValidator<Base64Image, St
         if (value == null) {
             return true;
         }
-        if (!isBase64Image(value)) {
+        if (!ImageUtil.isBase64Image(value)) {
             return false;
         }
 
-        var type = extractType(value);
+        var type = ImageUtil.extractBase64ImageType(value);
         if (!ImageType.isValidImageType(type)) {
             throw new IllegalArgumentException("Invalid image type: " + type);
         }
-        var bytes = Base64.getDecoder().decode(extractBytes(value));
+        var bytes = Base64.getDecoder().decode(ImageUtil.extractBase64ImagBytes(value));
         if(bytes.length > maxSize) {
             throw new IllegalArgumentException("Image size exceeds 10 Mb");
         }
         return true;
-    }
-
-    private boolean isBase64Image(String value) {
-        return BASE64_IMAGE_PATTERN.matcher(value).matches();
-    }
-
-    private String extractType(String value) {
-        int colonIndex = value.indexOf(":");
-        int semicolonIndex = value.indexOf(";");
-        return value.substring(colonIndex + 1, semicolonIndex);
-    }
-
-    private String extractBytes(String value) {
-        return value.substring(value.indexOf(",") + 1);
     }
 }
---
+++ src/main/java/org/example/webstore/image/ImageUtil.java
@@ -0,0 +1,29 @@
+package org.example.webstore.image;
+
+import java.util.regex.Pattern;
+import lombok.NonNull;
+
+public class ImageUtil {
+    private static final Pattern BASE64_IMAGE_PATTERN = Pattern.compile("data:image\\/[^;]+;base64[^\"]+");
+
+    public static String extractBase64ImageType(@NonNull String image) {
+        if (isBase64Image(image)) {
+            int colonIndex = image.indexOf(":");
+            int semicolonIndex = image.indexOf(";");
+            return image.substring(colonIndex + 1, semicolonIndex);
+        }
+        throw new IllegalArgumentException("invalid string format");
+    }
+
+    public static String extractBase64ImagBytes(@NonNull String image) {
+        if (isBase64Image(image)) {
+            int index = image.indexOf(":");
+            return image.substring(index + 1);
+        }
+        throw new IllegalArgumentException("invalid string format");
+    }
+
+    public static boolean isBase64Image(String value) {
+        return BASE64_IMAGE_PATTERN.matcher(value).matches();
+    }
+}
