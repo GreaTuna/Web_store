9fabb59495f504ad663b83a3bb045badf4da9e44
GreaTuna <great.tuna.2020@gmail.com>
2024-11-04 16:13:58 +0200

    added exceptions handling

---
+++ src/main/java/org/example/webstore/global/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package org.example.webstore.global;
+
+import jakarta.persistence.EntityNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<?> handleEntityNotFoundException() {
+        return ResponseEntity.notFound().build();
+    }
+}
