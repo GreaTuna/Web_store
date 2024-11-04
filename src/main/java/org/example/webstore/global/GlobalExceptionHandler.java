792e79e137647ceb9fba6621b2f34f068dc91c7a
GreaTuna <great.tuna.2020@gmail.com>
2024-11-04 17:18:32 +0200

    added illegal image type exception handling

--- src/main/java/org/example/webstore/global/GlobalExceptionHandler.java
+++ src/main/java/org/example/webstore/global/GlobalExceptionHandler.java
@@ -1,6 +1,7 @@
 package org.example.webstore.global;
 
 import jakarta.persistence.EntityNotFoundException;
+import org.example.webstore.global.exception.IllegalImageTypeException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -10,5 +11,10 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<?> handleEntityNotFoundException() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(IllegalImageTypeException.class)
+    public ResponseEntity<String> handleInvalidImageTypeException(IllegalImageTypeException e)  {
+        return ResponseEntity.badRequest().body(e.getMessage());
     }
 }
--- src/main/java/org/example/webstore/global/validation/annotation/Image/Base64ImageValidator.java
+++ src/main/java/org/example/webstore/global/validation/annotation/Image/Base64ImageValidator.java
@@ -3,6 +3,7 @@ package org.example.webstore.global.validation.annotation.Image;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import java.util.Base64;
+import org.example.webstore.global.exception.IllegalImageTypeException;
 import org.example.webstore.image.ImageConfig;
 import org.example.webstore.image.ImageType;
 import org.example.webstore.image.ImageUtil;
@@ -28,7 +29,7 @@ public class Base64ImageValidator implements ConstraintValidator<Base64Image, St
 
         var type = ImageUtil.extractBase64ImageType(value);
         if (!ImageType.isValidImageType(type)) {
-            throw new IllegalArgumentException("Invalid image type: " + type);
+            throw new IllegalImageTypeException("forbidden image type: " + type.substring(type.indexOf('/') + 1));
         }
         var bytes = Base64.getDecoder().decode(ImageUtil.extractBase64ImagBytes(value));
         if(bytes.length > maxSize) {
