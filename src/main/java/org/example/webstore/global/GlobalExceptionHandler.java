b31f70585cc703ff71c285540a9d63f0fa738841
GreaTuna <great.tuna.2020@gmail.com>
2024-11-05 23:09:53 +0200

    added size limit error handling

--- src/main/java/org/example/webstore/global/GlobalExceptionHandler.java
+++ src/main/java/org/example/webstore/global/GlobalExceptionHandler.java
@@ -2,6 +2,7 @@ package org.example.webstore.global;
 
 import jakarta.persistence.EntityNotFoundException;
 import java.util.List;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.example.webstore.global.exception.IllegalImageTypeException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
@@ -12,8 +13,20 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
+    public static class Details {
+        public String message;
+        public long actual ;
+        public long permitted;
+
+        public Details(FileSizeLimitExceededException e) {
+            this.message = e.getMessage();
+            this.actual = e.getActualSize();
+            this.permitted = e.getPermittedSize();
+        }
+    }
+
     @ExceptionHandler(EntityNotFoundException.class)
-    public ResponseEntity<?> handleEntityNotFoundException() {
+    public ResponseEntity<?> handleEntityNotFound() {
         return ResponseEntity.notFound().build();
     }
 
@@ -29,7 +42,12 @@ public class GlobalExceptionHandler {
     }
 
     @ExceptionHandler(IllegalImageTypeException.class)
-    public ResponseEntity<String> handleInvalidImageTypeException(IllegalImageTypeException e)  {
+    public ResponseEntity<String> handleInvalidImageType(IllegalImageTypeException e)  {
         return ResponseEntity.badRequest().body(e.getMessage());
+    }
+
+    @ExceptionHandler(FileSizeLimitExceededException.class)
+    public ResponseEntity<Details> handleSizeLimitExceeded(FileSizeLimitExceededException e)  {
+        return ResponseEntity.badRequest().body(new Details(e));
     }
 }
--- src/main/java/org/example/webstore/global/validation/annotation/Image/Base64ImageValidator.java
+++ src/main/java/org/example/webstore/global/validation/annotation/Image/Base64ImageValidator.java
@@ -3,6 +3,8 @@ package org.example.webstore.global.validation.annotation.Image;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 import java.util.Base64;
+import lombok.SneakyThrows;
+import org.apache.tomcat.util.http.fileupload.impl.FileSizeLimitExceededException;
 import org.example.webstore.global.exception.IllegalImageTypeException;
 import org.example.webstore.image.ImageConfig;
 import org.example.webstore.image.ImageType;
@@ -13,6 +15,7 @@ public class Base64ImageValidator implements ConstraintValidator<Base64Image, St
     @Autowired
     private ImageConfig imageConfig;
 
+    @SneakyThrows
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         if (value == null) {
@@ -28,7 +31,9 @@ public class Base64ImageValidator implements ConstraintValidator<Base64Image, St
         }
         var bytes = Base64.getDecoder().decode(ImageUtil.extractBase64ImagBytes(value));
         if(bytes.length > imageConfig.getMaxSizeInBytes()) {
-            throw new IllegalArgumentException("Image size exceeds " + imageConfig.getMaxSize() + " Mb");
+            double sizeInMB = bytes.length / 1024.0 / 1024.0;
+            var message = "Image size exceeds: actual " + String.format("%.1f", sizeInMB) + " Mb, permitted " + imageConfig.getMaxSize() + " Mb";
+            throw new FileSizeLimitExceededException(message, bytes.length , imageConfig.getMaxSizeInBytes());
         }
         return true;
     }
