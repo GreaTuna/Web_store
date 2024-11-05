006d08d1b906941ca3a1a2011a9db37adada8e39
GreaTuna <great.tuna.2020@gmail.com>
2024-11-05 16:33:19 +0200

    added validation error handling

--- src/main/java/org/example/webstore/global/GlobalExceptionHandler.java
+++ src/main/java/org/example/webstore/global/GlobalExceptionHandler.java
@@ -1,8 +1,12 @@
 package org.example.webstore.global;
 
 import jakarta.persistence.EntityNotFoundException;
+import java.util.List;
 import org.example.webstore.global.exception.IllegalImageTypeException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -11,6 +15,17 @@ public class GlobalExceptionHandler {
     @ExceptionHandler(EntityNotFoundException.class)
     public ResponseEntity<?> handleEntityNotFoundException() {
         return ResponseEntity.notFound().build();
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<List<?>> handleMethodArgumentNotValid(MethodArgumentNotValidException e) {
+        return ResponseEntity.badRequest().body(
+            e.getBindingResult().getAllErrors().stream().map(error -> {
+                var problemDetail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, error.getDefaultMessage());
+                problemDetail.setTitle("Constraint Violation");
+                return problemDetail;
+            }).toList()
+        );
     }
 
     @ExceptionHandler(IllegalImageTypeException.class)
