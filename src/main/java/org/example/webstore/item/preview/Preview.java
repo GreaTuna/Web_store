832ee8e86bd0152e8b1ca33d238f2ad78c1f45ae
GreaTuna <great.tuna.2020@gmail.com>
2024-11-07 22:25:16 +0200

    added column validation

--- src/main/java/org/example/webstore/item/preview/Preview.java
+++ src/main/java/org/example/webstore/item/preview/Preview.java
@@ -1,21 +1,28 @@
 package org.example.webstore.item.preview;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
 import org.example.webstore.image.Image;
 
+
 @Getter
 @Setter
+@Embeddable
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Preview {
-    @Size(max = 256)
+    @NonNull
     @NotBlank
+    @Size(max = 256)
+    @Column(nullable = false)
     private String title;
 
     @NotNull
     @NonNull
+    @JoinColumn(name = "preview_image_id")
+    @OneToOne(orphanRemoval = true, cascade = CascadeType.ALL)
     private Image image;
 }
