f8b93454c3a3adfdfddd36b436c118ba089f7360
GreaTuna <great.tuna.2020@gmail.com>
2024-11-10 22:40:09 +0200

    added values to categories enum

--- src/main/java/org/example/webstore/item/enums/Category.java
+++ src/main/java/org/example/webstore/item/enums/Category.java
@@ -1,10 +1,18 @@
 package org.example.webstore.item.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
 public enum Category {
-    CARS,
-    FREEBIES,
-    ELECTRONICS,
-    CHILDREN_GOODS,
-    CLOTHES_AND_SHOES,
-    SPORTS_AND_OUTDOOR_ACTIVITIES,
+    CARS("cars"),
+    FREEBIES("freebies"),
+    ELECTRONICS("electronics"),
+    CHILDREN_GOODS("children-goods"),
+    CLOTHES_AND_SHOES("clothes-and-shoes"),
+    SPORTS_AND_OUTDOOR_ACTIVITIES("sports-and-outdoor-activities"),
+    ;
+
+    private final String category;
 }
