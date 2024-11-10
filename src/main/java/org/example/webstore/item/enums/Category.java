3bbbe605df253388962026e916a49a863ded3c85
GreaTuna <great.tuna.2020@gmail.com>
2024-11-10 19:26:43 +0200

    added categories enum

---
+++ src/main/java/org/example/webstore/item/enums/Category.java
@@ -0,0 +1,10 @@
+package org.example.webstore.item.enums;
+
+public enum Category {
+    CARS,
+    FREEBIES,
+    ELECTRONICS,
+    CHILDREN_GOODS,
+    CLOTHES_AND_SHOES,
+    SPORTS_AND_OUTDOOR_ACTIVITIES,
+}
