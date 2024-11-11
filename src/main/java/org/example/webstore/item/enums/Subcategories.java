9317c656b4ab6e18d98997c584b017e4b81de9eb
GreaTuna <great.tuna.2020@gmail.com>
2024-11-11 14:33:22 +0200

    added values to subcategories enum

--- src/main/java/org/example/webstore/item/enums/Subcategories.java
+++ src/main/java/org/example/webstore/item/enums/Subcategories.java
@@ -1,29 +1,38 @@
 package org.example.webstore.item.enums;
 
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public enum Subcategories {
-    // ELECTRONICS
-    AUDIO_DEVICES,
-    PHONES_AND_ACCESSORIES,
-    COMPUTERS_AND_LAPTOPS,
-    MONITORS_AND_TELEVISIONS,
+    // electronics
+    AUDIO_DEVICES("audio_devices"),
+    COMPUTERS_AND_LAPTOPS("computers_and_laptops"),
+    PHONES_AND_ACCESSORIES("phones_and_accessories"),
+    MONITORS_AND_TELEVISIONS("monitors_and_televisions"),
 
-    // CHILDREN_GOODS
-    TOYS,
-    CHILDREN_CLOTHES,
+    // children-goods
+    TOYS("toys"),
+    CHILDREN_CLOTHES("children_clothes"),
 
-    // CARS
-    PASSENGER_CARS,
-    CARGO_VEHICLES,
-    MOTORCYCLES,
+    // cars
+    MOTORCYCLES("motorcycles"),
+    PASSENGER_CARS("passenger_cars"),
+    CARGO_VEHICLES("cargo_vehicles"),
 
-    // CLOTHES_AND_SHOES
-    SHOES,
-    MEN_CLOTHING,
-    WOMEN_CLOTHING,
+    // clothes-and-shoes
+    SHOES("shoes"),
+    MEN_CLOTHING("men_clothing"),
+    WOMEN_CLOTHING("women_clothing"),
 
-    // SPORTS_AND_OUTDOOR_ACTIVITIES
-    BICYCLES,
-    SPORTS_EQUIPMENT,
-    CAMPING_AND_HIKING
+    // sports-and-outdoor-activities
+    BICYCLES("bicycles"),
+    SPORTS_EQUIPMENT("sports_equipment"),
+    CAMPING_AND_HIKING("camping_and_hiking"),
+    ;
+
+    private final String subcategory;
 }
 
