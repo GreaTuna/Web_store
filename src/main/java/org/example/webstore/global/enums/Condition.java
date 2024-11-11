9803000a27ffbcaf6c42f7150a13c62b585bc6ec
GreaTuna <great.tuna.2020@gmail.com>
2024-11-12 01:23:33 +0200

    added method fromCondition

--- src/main/java/org/example/webstore/global/enums/Condition.java
+++ src/main/java/org/example/webstore/global/enums/Condition.java
@@ -1,13 +1,27 @@
 package org.example.webstore.global.enums;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.SneakyThrows;
 
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public enum Condition {
+public enum Condition implements EnumValueProvider {
     NEW("new"),
     USED("used"),
     ;
 
     public final String condition;
+
+    @Override
+    public String getEnumValue() {
+        return condition;
+    }
+
+    @SneakyThrows
+    public static Condition fromCondition(@NotBlank String condition) {
+        var value = EnumValueProvider.fromValue(condition, Condition.values());
+        if (value != null) {return value;}
+        throw new IllegalArgumentException("Invalid condition: " + condition);
+    }
 }
