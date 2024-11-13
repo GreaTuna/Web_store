d4fa3182a9a3bc14eb166fbfddb478e075ab033c
GreaTuna <great.tuna.2020@gmail.com>
2024-11-13 05:16:44 +0200

    refactor class Preview

--- src/main/java/org/example/webstore/item/preview/Preview.java
+++ src/main/java/org/example/webstore/item/preview/Preview.java
@@ -1,7 +1,6 @@
 package org.example.webstore.item.preview;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.*;
@@ -15,13 +14,10 @@ import org.example.webstore.image.Image;
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Preview {
     @NonNull
-    @NotBlank
+    @NotNull
     @Size(max = 256)
-    @Column(nullable = false)
     private String title;
 
-    @NotNull
-    @NonNull
     @JoinColumn(name = "preview_image_id")
     @OneToOne(orphanRemoval = true, cascade = CascadeType.ALL)
     private Image image;
