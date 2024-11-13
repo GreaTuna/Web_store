5eb4ef05f5a5bb9450f69c24858d15876e6c3464
GreaTuna <great.tuna.2020@gmail.com>
2024-11-13 06:39:38 +0200

    added fields: condition, category, subcategory

--- src/main/java/org/example/webstore/item/details/Item.java
+++ src/main/java/org/example/webstore/item/details/Item.java
@@ -6,19 +6,21 @@ import jakarta.validation.constraints.*;
 import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
-import lombok.NonNull;
+import lombok.*;
+import org.example.webstore.global.enums.Category;
+import org.example.webstore.global.enums.Condition;
+import org.example.webstore.global.enums.Subcategory;
 import org.example.webstore.image.Image;
+import org.example.webstore.item.CategoryMap;
 import org.example.webstore.item.preview.Preview;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.Immutable;
 
-@Valid
 @Entity(name = "item")
 @Table(name = "item", schema = "public")
-@AllArgsConstructor
+@Getter
+@Setter
+@RequiredArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Item {
     @Id
@@ -46,8 +48,34 @@ public class Item {
     @CreationTimestamp
     public Instant createdAt;
 
+    @Enumerated(EnumType.ORDINAL)
+    private Condition condition;
+
+    @NotNull
+    @NonNull
+    @Setter(AccessLevel.NONE)
+    @Enumerated(EnumType.ORDINAL)
+    private Category category;
+
+    @Setter(AccessLevel.NONE)
+    @Enumerated(EnumType.ORDINAL)
+    private Subcategory subcategory;
+
     @OneToMany(orphanRemoval = true, cascade = CascadeType.ALL)
     @JoinTable(schema = "public", name = "item_gallery")
     private List<Image> gallery = new ArrayList<>();
+
+
+    public void setCategory(@NonNull Category category) {
+        if (!CategoryMap.isCompatible(category, this.subcategory)) {
+            throw new IllegalArgumentException("Category " + category.getEnumValue() + " is not compatible with subcategory " + this.subcategory.getEnumValue());
+        }
+    }
+
+    public void setSubcategory(@NonNull Subcategory subcategory) {
+        if (!CategoryMap.isCompatible(this.category, subcategory)) {
+            throw new IllegalArgumentException("Category " + this.category.getEnumValue() + " is not compatible with subcategory " + subcategory.getEnumValue());
+        }
+    }
 }
 
