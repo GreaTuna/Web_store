12d43eb2577c517c468a741aa409f318203350b0
GreaTuna <great.tuna.2020@gmail.com>
2024-11-25 06:23:58 +0200

    added type use for annotation

--- src/main/java/org/example/webstore/global/validation/annotation/image/Base64Image.java
+++ src/main/java/org/example/webstore/global/validation/annotation/image/Base64Image.java
@@ -7,7 +7,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target({ElementType.FIELD, ElementType.PARAMETER})
+@Target({ElementType.FIELD, ElementType.PARAMETER,  ElementType.TYPE_USE})
 @Retention(RetentionPolicy.RUNTIME)
 @Constraint(validatedBy = Base64ImageValidator.class)
 public  @interface  Base64Image {
