fba64b2b1f71a7031953e52188ab78d09074230f
GreaTuna <great.tuna.2020@gmail.com>
2024-11-30 17:46:36 +0200

    added item controller

---
+++ src/main/java/org/example/webstore/item/details/ItemController.java
@@ -0,0 +1,24 @@
+package org.example.webstore.item.details;
+
+import lombok.RequiredArgsConstructor;
+import org.example.webstore.global.validation.groups.Post;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/items")
+public class ItemController {
+    private final ItemMapper itemMapper;
+    private final ItemService itemService;
+
+    @PostMapping
+    public ResponseEntity<?> add(@RequestBody @Validated(Post.class)  PostItemDTO itemDTO) {
+        itemService.save(itemMapper.toEntity(itemDTO));
+        return ResponseEntity.ok().build();
+    }
+}
