a6140a79854692200942fad2262bca01d1810dc9
GreaTuna <great.tuna.2020@gmail.com>
2024-12-01 21:39:02 +0200

    removed unused import

--- src/main/java/org/example/webstore/image/Image.java
+++ src/main/java/org/example/webstore/image/Image.java
@@ -1,7 +1,6 @@
 package org.example.webstore.image;
 
 import jakarta.persistence.*;
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.util.Base64;
 import lombok.*;
