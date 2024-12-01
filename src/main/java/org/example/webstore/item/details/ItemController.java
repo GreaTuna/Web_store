4769f1d78f50c9c012a08742a515b10519a49e60
GreaTuna <great.tuna.2020@gmail.com>
2024-12-01 22:43:46 +0200

    added getPreviews method

--- src/main/java/org/example/webstore/item/details/ItemController.java
+++ src/main/java/org/example/webstore/item/details/ItemController.java
@@ -1,14 +1,13 @@
 package org.example.webstore.item.details;
 
 import jakarta.validation.groups.Default;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.example.webstore.global.validation.groups.Post;
+import org.example.webstore.item.preview.GetPreviewDTO;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,10 +15,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class ItemController {
     private final ItemMapper itemMapper;
     private final ItemService itemService;
+    private final ItemRepository itemRepository;
 
     @PostMapping
     public ResponseEntity<?> add(@RequestBody @Validated({Default.class, Post.class}) PostItemDTO itemDTO) {
         itemService.save(itemMapper.toEntity(itemDTO));
         return ResponseEntity.ok().build();
+    }
+
+    @GetMapping("/previews")
+    public ResponseEntity<List<GetPreviewDTO>> getPreviews() {
+        return ResponseEntity.ok(
+            itemRepository.findAll().stream().map(itemMapper::toGetPreviewDTO).toList()
+        );
     }
 }
