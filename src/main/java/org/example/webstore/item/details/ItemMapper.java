a4fdca51e7a0af8a5a7bec2db6a83bc6e1844349
GreaTuna <great.tuna.2020@gmail.com>
2024-12-01 22:28:47 +0200

    removed PreviewMapper all methods moved to ItemMapper

--- src/main/java/org/example/webstore/item/details/ItemMapper.java
+++ src/main/java/org/example/webstore/item/details/ItemMapper.java
@@ -1,7 +1,9 @@
 package org.example.webstore.item.details;
 
 import org.example.webstore.image.ImageMapper;
-import org.example.webstore.item.preview.PreviewMapper;
+import org.example.webstore.item.preview.GetPreviewDTO;
+import org.example.webstore.item.preview.PostPreviewDTO;
+import org.example.webstore.item.preview.Preview;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
@@ -14,14 +16,19 @@ public abstract class ItemMapper {
     @Autowired
     protected ImageMapper imageMapper;
 
-    @SuppressWarnings("all")
-    @Autowired
-    protected PreviewMapper previewMapper;
+    @Mapping(target = "image", expression = "java(imageMapper.toEntity(dto.image()))")
+    public abstract Preview toPreviewEntity(PostPreviewDTO dto);
+
+    @Mapping(target = "title", expression = "java(item.getPreview().getTitle())")
+    @Mapping(target = "postingDate", expression = "java(item.getCreatedAt().toString())")
+    @Mapping(target = "itemCondition", expression = "java(item.getCondition().getEnumValue())")
+    @Mapping(target = "image", expression = "java(imageMapper.toGetDTO(item.getPreview().getImage()))")
+    public abstract GetPreviewDTO toGetPreviewDTO(Item item);
 
     @Mapping(target = "condition", expression = "java(Condition.fromCondition(dto.condition()))")
     @Mapping(target = "category", expression = "java(Category.fromCategory(dto.category()))")
     @Mapping(target = "subcategory", expression = "java(Subcategory.fromSubcategory(dto.subcategory()))")
-    @Mapping(target = "preview", expression = "java(previewMapper.toEntity(dto.preview()))")
+    @Mapping(target = "preview", expression = "java(toPreviewEntity(dto.preview()))")
     @Mapping(target = "gallery", expression = "java(dto.gallery().stream().map(imageMapper::toEntity).toList())")
     public abstract Item toEntity(PostItemDTO dto);
 }
