e8943d0ec7cd2af63629b21d6cbf98ef1ddf0bc2
GreaTuna <great.tuna.2020@gmail.com>
2024-12-01 21:34:22 +0200

    updated toGetDTO method

--- src/main/java/org/example/webstore/item/preview/PreviewMapper.java
+++ src/main/java/org/example/webstore/item/preview/PreviewMapper.java
@@ -1,6 +1,7 @@
 package org.example.webstore.item.preview;
 
 import org.example.webstore.image.ImageMapper;
+import org.example.webstore.item.details.Item;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.ReportingPolicy;
@@ -16,6 +17,9 @@ public abstract class PreviewMapper {
     @Mapping(target = "image", expression = "java(imageMapper.toEntity(dto.image()))")
     public abstract Preview toEntity(PostPreviewDTO dto);
 
-    @Mapping(target = "image", expression = "java(imageMapper.toGetDTO(preview.getImage()))")
-    public abstract GetPreviewDTO toGetDTO(Preview preview);
+    @Mapping(target = "title", expression = "java(item.getPreview().getTitle())")
+    @Mapping(target = "postingDate", expression = "java(item.getCreatedAt().toString())")
+    @Mapping(target = "itemCondition", expression = "java(item.getCondition().getEnumValue())")
+    @Mapping(target = "image", expression = "java(imageMapper.toGetDTO(item.getPreview().getImage()))")
+    public abstract GetPreviewDTO toGetDTO(Item item);
 }
